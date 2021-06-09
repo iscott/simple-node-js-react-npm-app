66a23a7938a123b5bd092b6d09bff903260cb4ab
Ira Herman <ira@xynetik.com>
2021-06-09 09:56:31 -0700

    Sets +x to show npm start command output

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -20,14 +20,11 @@ echo 'can pause running builds of CI/CD applications indefinitely. "npm start"'
 echo 'is followed by another command that retrieves the process ID (PID) value'
 echo 'of the previously run process (i.e. "npm start") and writes this value to'
 echo 'the file ".pidfile".'
-set -x
+# set -x
 npm start &
 sleep 1
 echo $! > .pidfile
-set +x
-
-echo '============ installing curl ============'
-apt-get -y install curl
+# set +x
 
 echo '============ doing a curl ============'
 curl http://0.0.0.0:3000
